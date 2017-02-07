5d7144e857c9d6e989a2c35741cf261861c365bf
Jumpei Anzai <junpei.anzai@gmail.com>
2017-02-07 14:30:54 +0900

    Add complete icon

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -2,13 +2,27 @@ import React from 'react';
 
 export default class Todo extends React.Component {
 
+    constructor(props) {
+        super();
+    }
+
     render () {
-        const {complete, text} = this.props;
+        const {complete, edit, text} = this.props;
+
+        const icon = complete ? '\u2714' : '\u2716';
+
+        if (edit) {
+            return (
+                <li>
+                    <input value={text} focus='focused'/>
+                </li>
+            );
+        }
 
         return (
             <li>
-                <span>{complete}</span>
                 <span>{text}</span>
+                <span>{icon}</span>
             </li>
         );
     }
